3c4f93f6d225e01c380a2a4d5b7f5947c6eefee6
Luke Morrill <lukemorrill@gmail.com>
2018-09-19 13:36:00 -0400

    Implemented RandomInput.

--- MLMCPy/input/RandomInput.py
+++ MLMCPy/input/RandomInput.py
@@ -1,11 +1,40 @@
+import numpy as np
 
 
 class RandomInput:
+    """
+    Used to draw samples from a specified distribution, with a uniform
+    distribution as the default. Any distribution function provided
+    must accept a "size" parameter that determines the sample size.
 
-    def __init__(self, data):
+    :param distribution_function function that returns a sample of a distribution
+        with the sample sized determined by a "size" parameter.
+    :type function
+    :param distribution_function_args any arguments required by the distribution
+        function, with the exception of "size", which will be provided to the
+        function when draw_samples is called.
+    """
+    def __init__(self, distribution_function=np.random.uniform,
+                 **distribution_function_args):
 
-        pass
+        if not callable(distribution_function):
+            raise TypeError('distribution_function must be a function.')
+
+        self.distribution = distribution_function
+        self.args = distribution_function_args
 
     def draw_samples(self, num_samples):
+        """
+        Returns num_samples samples from a distribution in the form of a
+        numpy array.
+        :param num_samples: Size of array to return.
+        :type int
+        :return: ndarray of distribution sample.
+        """
 
-        pass
+        if not isinstance(num_samples, int):
+            raise TypeError("num_samples must be an integer.")
+
+        # Pass in num_samples as size argument to distribution function.
+        self.args['size'] = num_samples
+        return self.distribution(**self.args)
--- tests/input/test_RandomInput.py
+++ tests/input/test_RandomInput.py
@@ -1,16 +1,56 @@
 import pytest
+import numpy as np
 
 from MLMCPy.input import RandomInput
+
+
+@pytest.fixture
+def uniform_distribution_input():
+
+    return RandomInput(np.random.uniform)
+
+
+@pytest.fixture
+def invalid_distribution_function():
+
+    def invalid_function():
+        return [1, 2, 3]
+
+    return invalid_function
 
 
 def test_init_invalid_input():
 
     with pytest.raises(TypeError):
-        RandomInput([])
         RandomInput(1)
-        RandomInput('five')
 
-    with pytest.raises(ValueError):
-        RandomInput(np.zeros(0))
+
+def test_draw_samples_expected_output(uniform_distribution_input):
+
+    sample = uniform_distribution_input.draw_samples(5)
+    assert isinstance(sample, np.ndarray)
+    assert sample.shape == (5,)
+
+
+def test_exception_invalid_distribution_function(invalid_distribution_function):
+
+    with pytest.raises(TypeError):
+        invalid_sampler = RandomInput(invalid_distribution_function)
+        invalid_sampler.draw_samples(5)
+
+
+def test_extra_distribution_function_parameters():
+
+    normal_sampler = RandomInput(np.random.normal, loc=1., scale=2.0)
+    sample = normal_sampler.draw_samples(5)
+
+    assert isinstance(sample, np.ndarray)
+    assert sample.shape == (5,)
+
+
+def test_draw_samples_invalid_arguments(uniform_distribution_input):
+
+    with pytest.raises(TypeError):
+        uniform_distribution_input.draw_samples(1.)
 
 
