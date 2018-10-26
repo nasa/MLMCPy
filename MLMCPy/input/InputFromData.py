d3d7801ba15ef908ecbbc49cf1c6569a7aa9bcb8
Luke Morrill <lukemorrill@gmail.com>
2018-10-26 13:22:02 -0400

    Completed first draft of documentation intro page.

--- MLMCPy/input/InputFromData.py
+++ MLMCPy/input/InputFromData.py
@@ -14,7 +14,7 @@ class InputFromData(Input):
                  shuffle_data=True):
         """
         :param input_filename: path of file containing data to be sampled.
-        :type string
+        :type input_filename: string
         :param delimiter: Character used to separate data in data file.
             Can also be an integer to specify width of each entry.
         :type delimiter: str or int
@@ -52,7 +52,7 @@ class InputFromData(Input):
         Returns an array of samples from the previously loaded file data.
 
         :param num_samples: Number of samples to be returned.
-        :type int
+        :type num_samples: int
         :return: 2d ndarray of samples, each row being one sample.
                  For one dimensional input data, this will have
                  shape (num_samples, 1)
