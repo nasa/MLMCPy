Fixed issue with computation of initial variances in MPI environments. Also ensured all samples from data are available in MPI environments via InputFromData. Created unit tests to test both issues MPI. Now testing output estimates, samples per level, and variances in MPI environments vs single processor baseline as well.

MLMCPy/input/InputFromData.py
@@ -41,7 +41,7 @@ class InputFromData(Input):
             self._data = self._data.reshape(self._data.shape[0], -1)
 
         # Use subset of data if we are in multiprocessor environment.
-        self.__detect_parallelization()
+        self._detect_parallelization()
 
         if shuffle_data:
             np.random.shuffle(self._data)
@@ -87,29 +87,37 @@ class InputFromData(Input):
         """
         self._index = 0
 
-    def __detect_parallelization(self):
+    def _detect_parallelization(self, num_cpus=1, cpu_rank=0, override=False):
         """
         If multiple cpus detected, split the data across cpus so that
         each will have a unique subset of sample data.
         """
-        num_cpus = 1
-        cpu_rank = 0
 
         try:
-            imp.find_module('mpi4py')
+            if not override:
 
-            from mpi4py import MPI
-            comm = MPI.COMM_WORLD
+                imp.find_module('mpi4py')
 
-            cpu_rank = comm.rank
-            num_cpus = comm.size
+                from mpi4py import MPI
+                comm = MPI.COMM_WORLD
+
+                cpu_rank = comm.rank
+                num_cpus = comm.size
 
         except ImportError:
+
             num_cpus = 1
             cpu_rank = 0
 
         finally:
+
+            total_num_samples = self._data.shape[0]
             slice_size = self._data.shape[0] // num_cpus
+
+            num_residual_samples = total_num_samples - slice_size * num_cpus
+
+            if cpu_rank < num_residual_samples:
+                slice_size += 1
 
             slice_start_index = slice_size * cpu_rank
             self._data = self._data[slice_start_index:

MLMCPy/mlmc/MLMCSimulator.py
@@ -271,7 +271,6 @@ class MLMCSimulator:
         # Variance is computed from difference between outputs of adjacent
         # layers evaluated from the same samples.
         compute_times = np.zeros(self._num_levels)
-        variances = np.zeros((self._num_levels, self._output_size))
 
         for level in range(self._num_levels):
 
@@ -285,17 +284,19 @@ class MLMCSimulator:
                 self._cache[level, i] = self._models[level].evaluate(sample)
 
                 if level > 0:
-                    lower_level_outputs[i] = self._models[level-1].evaluate(sample)
+                    lower_level_outputs[i] = \
+                        self._models[level-1].evaluate(sample)
 
             compute_times[level] = timeit.default_timer() - start_time
 
             self._cache[level] -= lower_level_outputs
-            variances[level] = np.var(self._cache[level], axis=0)
+
+        # Get outputs across all CPUs before computing variances.
+        all_outputs = self._concatenate_arrays_over_all_cpus(self._cache,
+                                                             axis=1)
+        variances = np.var(all_outputs, axis=1)
 
         costs = self._compute_costs(compute_times)
-
-        costs = self._mean_over_all_cpus(costs)
-        variances = self._mean_over_all_cpus(variances)
 
         if self._verbose and self._cpu_rank == 0:
             print 'Initial sample variances: \n%s' % variances
@@ -326,6 +327,12 @@ class MLMCSimulator:
         else:
             # Compute costs based on compute time differences between levels.
             costs = compute_times / self._initial_sample_size
+
+            # If costs are determined by time, we also need to multiply them
+            # by the number of cpus so that they are based on total cost.
+            costs *= self._num_cpus
+
+        costs = self._mean_over_all_cpus(costs)
 
         # Save copy of costs for use in simulation as is.
         self._costs = np.copy(costs)
@@ -533,6 +540,18 @@ class MLMCSimulator:
 
         return np.mean(all_values, 0)
 
+    def _concatenate_arrays_over_all_cpus(self, arrays, axis=0):
+
+        if self._num_cpus == 1:
+            return arrays
+
+        from mpi4py import MPI
+        comm = MPI.COMM_WORLD
+
+        all_arrays = comm.allgather(arrays)
+
+        return np.concatenate(all_arrays, axis=axis)
+
     def _determine_num_cpu_samples(self, total_num_samples):
         """Determines number of samples to be run on current cpu based on
             total number of samples to be run.
@@ -541,7 +560,7 @@ class MLMCSimulator:
         """
         num_cpu_samples = total_num_samples // self._num_cpus
         num_residual_samples = total_num_samples - \
-                               num_cpu_samples * self._num_cpus
+            num_cpu_samples * self._num_cpus
 
         if self._cpu_rank < num_residual_samples:
             num_cpu_samples += 1

tests/input/test_InputFromData.py
@@ -1,6 +1,5 @@
 import pytest
 import os
-import imp
 import numpy as np
 
 from MLMCPy.input import InputFromData
@@ -24,13 +23,13 @@ for data_file in data_file_names:
 # Pull all scrambled sample data from a file as one ndarray.
 def get_full_data_set(file_path):
 
-    filename = os.path.basename(file_path)
-    file_length = data_file_lengths[filename]
+    return np.genfromtxt(file_path)
 
-    data_sampler = InputFromData(file_path)
-    full_data_set = data_sampler.draw_samples(file_length)
 
-    return full_data_set
+def get_data_file_size(file_path):
+
+    file_name = os.path.basename(file_path)
+    return data_file_lengths[file_name]
 
 
 @pytest.fixture
@@ -44,21 +43,6 @@ def bad_data_file():
 
     data_file_with_bad_data = os.path.join(data_path, "bad_data.txt")
     return data_file_with_bad_data
-
-
-@pytest.fixture
-def mpi_info():
-    try:
-        imp.find_module('mpi4py')
-
-        from mpi4py import MPI
-        comm = MPI.COMM_WORLD
-
-        return comm.size, comm.rank
-
-    except ImportError:
-
-        return 1, 0
 
 
 def test_init_fails_on_invalid_input_file():
@@ -115,19 +99,20 @@ def test_draw_samples_pulls_all_input_data(data_filename):
     file_data = np.genfromtxt(data_filename)
     file_data = file_data.reshape(file_data.shape[0], -1)
 
-    assert all_sampled_data.shape == file_data.shape
+    assert all_sampled_data.shape[0] == file_data.shape[0]
 
 
 @pytest.mark.parametrize("data_filename", data_file_paths, ids=data_file_names)
 def test_sample_data_is_scrambled(data_filename):
 
-    all_sampled_data = get_full_data_set(data_filename)
+    all_file_data = get_full_data_set(data_filename)
+    file_length = all_file_data.shape[0]
 
-    file_data = np.genfromtxt(data_filename)
-    file_data = file_data.reshape(file_data.shape[0], -1)
+    data_sampler = InputFromData(data_filename)
+    sample_data = data_sampler.draw_samples(file_length)
 
-    assert not np.array_equal(all_sampled_data, file_data)
-    assert np.isclose(np.sum(all_sampled_data), np.sum(file_data))
+    assert not np.array_equal(all_file_data, sample_data)
+    assert np.isclose(np.sum(all_file_data), np.sum(sample_data))
 
 
 @pytest.mark.parametrize("data_filename", data_file_paths, ids=data_file_names)
@@ -171,24 +156,31 @@ def test_draw_sample_warning_issued_for_insufficient_data(data_filename_2d):
 
 
 @pytest.mark.parametrize("data_filename", data_file_paths, ids=data_file_names)
-def test_mpi_input_sample_sliced(mpi_info, data_filename):
+def test_multi_cpu_input_sample_slicing(data_filename):
 
-    # This is an MPI only test, so pass if we're running in single cpu mode.
-    if mpi_info == (1, 0):
-        return
+    all_data_size = get_data_file_size(data_filename)
 
-    num_cpus, cpu_rank = mpi_info
+    for num_cpus in [1, 2, 3, 4, 7]:
 
-    # Get expected slice.
-    data_input = InputFromData(data_filename)
-    all_data = get_full_data_set(data_filename)
+        slice_sizes = list()
 
-    slice_size = all_data.shape[0] // num_cpus
+        for cpu_rank in range(num_cpus):
 
-    slice_start_index = slice_size * cpu_rank
-    sliced_data = all_data[slice_start_index: slice_start_index + slice_size]
+            # Force data_input to slice as if in MPI environment.
+            data_input = InputFromData(data_filename)
+            data_input._detect_parallelization(num_cpus, cpu_rank, True)
 
-    input_data = data_input._data
+            # Get expected slice size.
+            slice_size = all_data_size // num_cpus
+            num_residual_samples = all_data_size - slice_size * num_cpus
 
-    # Ensure InputFromData sliced the data in the same way.
-    assert np.array_equal(sliced_data, input_data)
+            if cpu_rank < num_residual_samples:
+                slice_size += 1
+
+            slice_sizes.append(slice_size)
+
+            # Ensure InputFromData sliced the data in the same way.
+            assert np.array_equal(slice_size, data_input._data.shape[0])
+
+        # Ensure all data is going to be used.
+        assert np.sum(slice_sizes) == all_data_size

tests/mlmc/test_MLMCSimulator.py
@@ -103,21 +103,6 @@ def filename_2d_3_column_data():
     return os.path.join(data_path, "2D_test_data_output_3_col.csv")
 
 
-@pytest.fixture
-def mpi_info():
-    try:
-        imp.find_module('mpi4py')
-
-        from mpi4py import MPI
-        comm = MPI.COMM_WORLD
-
-        return comm.size, comm.rank
-
-    except ImportError:
-
-        return 1, 0
-
-
 def test_model_from_data(data_input, models_from_data):
 
     sim = MLMCSimulator(models=models_from_data, data=data_input)
@@ -522,30 +507,6 @@ def test_mc_output_shapes_match_mlmc(data_input, models_from_data):
     assert mc_sample_sizes.shape != mlmc_sample_sizes.shape
 
 
-@pytest.mark.parametrize('num_cpus', [1, 2, 3, 4, 7, 12])
-def test_multi_cpu_sample_sizing(data_input, models_from_data, num_cpus):
-
-    total_samples = 100
-
-    sample_sizes = np.zeros(num_cpus)
-
-    sim = MLMCSimulator(models=models_from_data, data=data_input)
-
-    for cpu_rank in range(num_cpus):
-
-        sim._num_cpus = num_cpus
-        sim._cpu_rank = cpu_rank
-
-        sample_sizes[cpu_rank] = sim._determine_num_cpu_samples(total_samples)
-
-    # Test that all samples will be utilized.
-    assert np.sum(sample_sizes) == total_samples
-
-    # Test that there is never more than a difference of one sample
-    # between processes.
-    assert np.max(sample_sizes) - np.min(sample_sizes) <= 1
-
-
 def test_hard_coded_test_2_level(data_input, models_from_data):
 
     # Get simulation results.
@@ -651,27 +612,79 @@ def test_fixed_cost(beta_distribution_input, spring_models, target_cost):
 
     # We should be less than or at least very close to the target.
     # TODO: Try to reduce this overrun with profiling/refactoring.
-    assert compute_time < target_cost * 1.1
+    assert compute_time < target_cost * 1.2
 
 
-def test_mpi_random_input_unique_per_cpu(mpi_info, beta_distribution_input,
-                                         spring_models):
+@pytest.mark.parametrize('num_cpus', [1, 2, 3, 4, 7, 12])
+def test_multi_cpu_sample_splitting(data_input, models_from_data, num_cpus):
 
-    # This is an MPI only test, so pass if we're running in single cpu mode.
-    if mpi_info == (1, 0):
+    total_samples = 100
+
+    sample_sizes = np.zeros(num_cpus)
+
+    sim = MLMCSimulator(models=models_from_data, data=data_input)
+
+    for cpu_rank in range(num_cpus):
+
+        sim._num_cpus = num_cpus
+        sim._cpu_rank = cpu_rank
+
+        sample_sizes[cpu_rank] = sim._determine_num_cpu_samples(total_samples)
+
+    # Test that all samples will be utilized.
+    assert np.sum(sample_sizes) == total_samples
+
+    # Test that there is never more than a difference of one sample
+    # between processes.
+    assert np.max(sample_sizes) - np.min(sample_sizes) <= 1
+
+
+def test_multiple_cpu(data_input, models_from_data):
+
+    try:
+        imp.find_module('mpi4py')
+
+        from mpi4py import MPI
+        comm = MPI.COMM_WORLD
+
+    except ImportError:
+
         return
 
-    # Allow simulator to set up sampling and draw a sample.
-    sim = MLMCSimulator(models=spring_models, data=beta_distribution_input)
-    sim_data = sim._data.draw_samples(10)
+    # Set up baseline simulation like single processor run.
+    data_filename = os.path.join(data_path, "spring_mass_1D_inputs.txt")
+    full_data_input = InputFromData(data_filename)
+    full_data_input._data = np.genfromtxt(data_filename)
 
-    # Share data across cpus and compare to ensure each cpu has unique samples.
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
+    base_sim = MLMCSimulator(models=models_from_data, data=full_data_input)
+    base_sim._num_cpus = 1
+    base_sim._cpu_rank = 0
 
-    all_sim_data = comm.allgather(sim_data)
+    base_estimate, base_sample_sizes, base_variances = \
+        base_sim.simulate(.1, 200)
 
-    assert len(all_sim_data) == 2
+    base_costs, base_initial_variances = base_sim._compute_costs_and_variances()
 
-    for cpu_data in all_sim_data:
-        assert not np.array_equal(all_sim_data[0], cpu_data)
+    sim = MLMCSimulator(models=models_from_data, data=data_input)
+    estimates, sample_sizes, variances = sim.simulate(.1, 200)
+
+    sim_costs, initial_variances = sim._compute_costs_and_variances()
+
+    assert np.array_equal(base_initial_variances, initial_variances)
+    assert np.array_equal(base_costs, sim_costs)
+
+    all_estimates = comm.allgather(estimates)
+    all_sample_sizes = comm.allgather(sample_sizes)
+    all_variances = comm.allgather(variances)
+
+    for estimate in all_estimates:
+        assert np.all(np.isclose(estimate, base_estimate))
+
+    for variance in all_variances:
+        assert np.all(np.isclose(variance, base_variances))
+
+    every_sample_size = np.zeros((comm.size, 3))
+    for i, sample_size in enumerate(all_sample_sizes):
+        every_sample_size[i] = sample_size
+
+    assert np.array_equal(np.sum(every_sample_size, axis=0), base_sample_sizes)