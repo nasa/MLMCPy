e65029d5ccb7955a46ab7121e5f75b8d3b19def3
Luke Morrill <lukemorrill@gmail.com>
2018-12-04 12:30:46 -0500

    Enhanced testing of CDF Wrapper functionality.

--- tests/model/test_CDFWrapperModel.py
+++ tests/model/test_CDFWrapperModel.py
@@ -17,6 +17,35 @@ data_path = my_path + "/../testing_data"
 
 
 @pytest.fixture
+def data_input():
+    """
+    Creates an InputFromData object that produces samples from a file
+    containing spring mass input data.
+    """
+    return InputFromData(os.path.join(data_path, "spring_mass_1D_inputs.txt"),
+                         shuffle_data=False)
+
+
+@pytest.fixture
+def models_from_data():
+    """
+    Creates a list of three ModelFromData objects of increasing fidelity.
+    :return:
+    """
+    input_filepath = os.path.join(data_path, "spring_mass_1D_inputs.txt")
+    output1_filepath = os.path.join(data_path, "spring_mass_1D_outputs_1.0.txt")
+    output2_filepath = os.path.join(data_path, "spring_mass_1D_outputs_0.1.txt")
+    output3_filepath = os.path.join(data_path,
+                                    "spring_mass_1D_outputs_0.01.txt")
+
+    model1 = ModelFromData(input_filepath, output1_filepath, cost=1.)
+    model2 = ModelFromData(input_filepath, output2_filepath, cost=4.)
+    model3 = ModelFromData(input_filepath, output3_filepath, cost=16.)
+
+    return [model1, model2, model3]
+
+
+@pytest.fixture
 def beta_distribution_input():
 
     np.random.seed(1)
@@ -44,26 +73,15 @@ def spring_models():
 
 
 @pytest.fixture
-def grid():
+def grid_0_1():
 
     return np.arange(0., 1., .1)
 
 
-@pytest.fixture
-def distribution_functions(spring_models):
-
-    distribution_function_list = list()
-    for spring_model in spring_models:
-        distribution_function_list.append(CDFWrapperModel(spring_model,
-                                                          grid))
-
-    return distribution_function_list
-
-
-def test_init_fails_on_bad_parameters(spring_model, grid):
+def test_init_fails_on_bad_parameters(spring_model, grid_0_1):
 
     with pytest.raises(TypeError):
-        CDFWrapperModel("Model", grid)
+        CDFWrapperModel("Model", grid_0_1)
 
     with pytest.raises(TypeError):
         CDFWrapperModel(spring_model, "Grid")
@@ -75,44 +93,130 @@ def test_init_fails_on_bad_parameters(spring_model, grid):
         CDFWrapperModel(spring_model, np.array([[1, 2], [3, 4]]))
 
     with pytest.raises(TypeError):
-        CDFWrapperModel(spring_model, grid, "Super smooth")
+        CDFWrapperModel(spring_model, grid_0_1, "Super smooth")
 
 
 @pytest.mark.parametrize('sample', [0, -1, .5, 2.])
-def test_single_indicator(grid, sample):
+def test_single_indicator(grid_0_1, sample):
 
     inner_model = ModelForTesting('repeat')
-    distribution_function = CDFWrapperModel(inner_model, grid)
+    cdfw = CDFWrapperModel(inner_model, grid_0_1)
 
-    output = distribution_function.evaluate(sample)
+    output = cdfw.evaluate(sample)
 
-    expected_output_sum = np.count_nonzero(sample <= grid)
+    expected_output_sum = np.count_nonzero(sample <= grid_0_1)
 
+    # Ensure sum of value matches.
+    assert np.array_equal(np.sum(output), expected_output_sum)
+
+    # Ensure all values are either 0 or 1.
+    assert np.all(np.logical_or(output == 0, output == 1))
+
+
+@pytest.mark.parametrize('model_num', [0, 1, 2])
+def test_single_indicator_on_data(data_input, models_from_data, model_num):
+
+    grid = np.linspace(8, 25, 100)
+
+    cdfw = CDFWrapperModel(models_from_data[model_num], grid)
+
+    input_sample = np.squeeze(data_input.draw_samples(1))
+    output_sample = models_from_data[model_num].evaluate(input_sample)
+
+    output = cdfw.evaluate(input_sample)
+
+    expected_output_sum = np.count_nonzero(output_sample <= grid)
+
+    # Ensure all values are either 0 or 1.
+    assert np.all(np.logical_or(output == 0, output == 1))
+
+    # Ensure sum of value matches.
     assert np.array_equal(np.sum(output), expected_output_sum)
 
 
-def test_evaluate_returns_expected_results(grid):
+@pytest.mark.parametrize('model_num', [0, 1, 2])
+def test_single_cdf_wrapper_output_consistency(data_input, models_from_data,
+                                               model_num):
 
-    inner_model = ModelForTesting('repeat')
+    grid_size = 100
+    num_samples = 400
+    grid = np.linspace(8, 25, grid_size)
 
-    num_samples = 10
+    cdfw = CDFWrapperModel(models_from_data[model_num], grid)
 
-    distribution_function = CDFWrapperModel(inner_model, grid)
+    input_samples = data_input.draw_samples(num_samples)
+    outputs1 = np.zeros((num_samples, grid_size))
+    outputs2 = np.zeros_like(outputs1)
+
+    for i, sample in enumerate(input_samples):
+        outputs1[i] = cdfw.evaluate(sample)
+
+    for i, sample in enumerate(input_samples):
+        outputs2[i] = cdfw.evaluate(sample)
+
+    assert np.array_equal(outputs1, outputs2)
+
+
+@pytest.mark.parametrize('initial_sample_size', [100, 250, 500])
+def test_sim_result_consistency(data_input, models_from_data,
+                                initial_sample_size):
+
+    grid_size = 100
+    grid = np.linspace(8, 25, grid_size)
+
+    cdfw_level1 = CDFWrapperModel(models_from_data[0], grid)
+    cdfw_level2 = CDFWrapperModel(models_from_data[1], grid)
+    cdfw_level3 = CDFWrapperModel(models_from_data[2], grid)
+
+    cdfws = [cdfw_level1, cdfw_level2, cdfw_level3]
+
+    # Run simulation to generate cache.
+    sim = MLMCSimulator(data_input, cdfws)
+
+    cdf1, ss1, v1 = sim.simulate(epsilon=.05,
+                                 initial_sample_size=initial_sample_size)
+
+    cdf2, ss2, v2 = sim.simulate(epsilon=.05,
+                                 initial_sample_size=initial_sample_size)
+
+    assert np.all(np.isclose(ss1, ss2))
+    assert np.all(np.isclose(v1, v2))
+    assert np.all(np.isclose(cdf1, cdf2))
+
+
+@pytest.mark.parametrize('num_samples', [1, 2, 5, 500])
+def test_evaluate_returns_expected_results_from_data(data_input, num_samples,
+                                                     models_from_data):
+    grid = np.linspace(8, 25, 100)
+
+    cdfw = CDFWrapperModel(models_from_data[0], grid)
 
     outputs = np.zeros((num_samples, grid.size))
-    samples = np.arange(0., 1., 1. / num_samples)
+    input_samples = data_input.draw_samples(num_samples)
 
-    for i, sample in enumerate(samples):
-        outputs[i] = distribution_function.evaluate(sample)
+    for i, sample in enumerate(input_samples):
+        outputs[i] = cdfw.evaluate(sample)
 
-    estimate = np.sum(outputs, axis=0) / num_samples
+    cdf = np.sum(outputs, axis=0) / num_samples
 
-    expected_result = np.array([.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.])
-    assert np.array_equal(estimate, expected_result)
+    # Ensure all indicators are either 0 or 1.
+    assert np.all(np.logical_or(outputs == 0, outputs == 1))
+
+    # Verify that no negative values exist.
+    assert np.all(cdf >= 0.)
+
+    # Verify that cdf is monotonic.
+    assert np.all(cdf[1: -1] >= cdf[0: -2])
+
+    # Verify that area under function sums to one.
+    cdf_sum = np.sum(cdf[1:-1] - cdf[:-2])
+    assert np.isclose(cdf_sum, 1., atol=.01)
 
 
 @pytest.mark.parametrize('num_data_points', [100, 250, 1000])
-def test_sim_evaluate_returns_expected_results(num_data_points):
+@pytest.mark.parametrize('initial_sample_size', [10, 200, 500])
+def test_sim_evaluate_returns_expected_results(num_data_points,
+                                               initial_sample_size):
 
     grid = np.arange(0, 100)
 
@@ -126,90 +230,81 @@ def test_sim_evaluate_returns_expected_results(num_data_points):
 
     sim = MLMCSimulator(data_input, models)
     cdf, sample_counts, variances = \
-        sim.simulate(epsilon=.05, initial_sample_size=500)
+        sim.simulate(epsilon=.05, initial_sample_size=initial_sample_size)
 
     # Verify that no negative values exist.
     assert np.all(cdf >= 0.)
 
     # Verify that cdf is monotonic.
-    for i in range(1, len(cdf)):
-
-        if cdf[i] < cdf[i-1]:
-            assert False
+    assert np.all(cdf[1: -1] >= cdf[0: -2])
 
     # Verify that area under function sums to one.
     cdf_sum = np.sum(cdf[1:-1] - cdf[:-2])
     assert np.isclose(cdf_sum, 1., atol=.05)
 
 
-@pytest.mark.parametrize('num_data_points', [100, 250, 1000])
-def test_compare_cdf_sim_cached_uncached_results(num_data_points):
+@pytest.mark.parametrize('initial_sample_size', [250, 300, 350, 400])
+def test_output_caching_cdf_wrapper(initial_sample_size, data_input,
+                                    models_from_data):
+    """
+    Runs simulator's _evaluate_sample() with and without caching enabled
+    to ensure consistency of outputs when running a cdf wrapper model. Also
+    tests the estimate and variances with and without caching.
+    """
+    grid_size = 100
+    grid = np.linspace(8, 25, grid_size)
 
-    # Set up CDF model.
-    grid = np.arange(0, 100)
+    cdfw_level1 = CDFWrapperModel(models_from_data[0], grid)
+    cdfw_level2 = CDFWrapperModel(models_from_data[1], grid)
+    cdfw_level3 = CDFWrapperModel(models_from_data[2], grid)
 
-    inner_model = ModelForTesting('repeat')
-    distribution_function1 = CDFWrapperModel(inner_model, grid)
-    distribution_function2 = CDFWrapperModel(inner_model, grid)
-    models = [distribution_function1, distribution_function2]
+    cdfws = [cdfw_level1, cdfw_level2, cdfw_level3]
 
-    # Hard code costs so that we can expect consistent sample size results.
-    distribution_function1.cost = 1
-    distribution_function2.cost = 10
+    # Run simulation to generate cache.
+    cached_sim = MLMCSimulator(data_input, cdfws)
+    cached_cdf, cached_sample_sizes, cached_variances = \
+        cached_sim.simulate(epsilon=.05,
+                            initial_sample_size=initial_sample_size)
 
-    data = np.arange(0, num_data_points)
-    data_input = InputForTesting(data)
+    cached_inputs_caching = np.copy(cached_sim._cached_inputs)
+    cached_outputs_caching = np.copy(cached_sim._cached_outputs)
 
-    # Create and run uncached simulation.
-    uncached_sim = MLMCSimulator(data_input, models)
+    # Collect same data with caching disabled.
+    uncached_sim = MLMCSimulator(data_input, cdfws)
     uncached_sim._caching_enabled = False
-    uncached_cdf, uncached_sample_counts, uncached_variances = \
-        uncached_sim.simulate(epsilon=.05, initial_sample_size=500)
+    uncached_cdf, uncached_sample_sizes, uncached_variances = \
+        uncached_sim.simulate(epsilon=.05,
+                              initial_sample_size=initial_sample_size)
 
-    # Create and run cached simulation.
-    data_input = InputForTesting(data)
+    cached_inputs_no_caching = np.copy(uncached_sim._cached_inputs)
+    cached_outputs_no_caching = np.copy(uncached_sim._cached_outputs)
 
-    cached_sim = MLMCSimulator(data_input, models)
-    cached_cdf, cached_sample_counts, cached_variances = \
-        cached_sim.simulate(epsilon=.05, initial_sample_size=500)
+    assert np.array_equal(cached_inputs_caching, cached_inputs_no_caching)
+    assert np.array_equal(cached_outputs_caching, cached_outputs_no_caching)
 
-    # Test equality of all outputs.
-    assert np.all(cached_sample_counts == uncached_sample_counts)
-    assert np.all(cached_variances == uncached_variances)
-    assert np.all(cached_cdf == uncached_cdf)
+    # Now compare estimator and output variances.
+    # If caching is working properly, they should match.
+    assert np.array_equal(cached_sample_sizes, uncached_sample_sizes)
+    assert np.array_equal(cached_cdf, uncached_cdf)
+    assert np.array_equal(cached_variances, uncached_variances)
 
 
-@pytest.mark.parametrize('initial_sample_size', [20, 100, 250, 1000])
-def test_sim_cdf_from_data(initial_sample_size):
+@pytest.mark.parametrize('initial_sample_size', [10, 250, 500])
+def test_sim_evaluate_returns_expected_results_from_data(initial_sample_size,
+                                                         data_input,
+                                                         models_from_data):
 
-    # Define I/O files
-    inputfile = os.path.join(data_path, "spring_mass_1D_inputs.txt")
+    grid = np.linspace(8, 25, 10)
 
-    outputfile_level1 = os.path.join(data_path,
-                                     "spring_mass_1D_outputs_1.0.txt")
-    outputfile_level2 = os.path.join(data_path,
-                                     "spring_mass_1D_outputs_0.1.txt")
-    outputfile_level3 = os.path.join(data_path,
-                                     "spring_mass_1D_outputs_0.01.txt")
-
-    # Initialize random input & model objects
-    data_input = InputFromData(inputfile, shuffle_data=False)
-
-    model_level1 = ModelFromData(inputfile, outputfile_level1, cost=1.0)
-    model_level2 = ModelFromData(inputfile, outputfile_level2, cost=10.0)
-    model_level3 = ModelFromData(inputfile, outputfile_level3, cost=100.0)
-
-    grid = np.linspace(8, 25, 100)
-
-    cdfw_level1 = CDFWrapperModel(model_level1, grid)
-    cdfw_level2 = CDFWrapperModel(model_level2, grid)
-    cdfw_level3 = CDFWrapperModel(model_level3, grid)
+    cdfw_level1 = CDFWrapperModel(models_from_data[0], grid)
+    cdfw_level2 = CDFWrapperModel(models_from_data[1], grid)
+    cdfw_level3 = CDFWrapperModel(models_from_data[2], grid)
 
     cdfws = [cdfw_level1, cdfw_level2, cdfw_level3]
 
     mlmc_simulator = MLMCSimulator(data_input, cdfws)
     mlmc_simulator._caching_enabled = False
-    [cdf, sample_sizes, variances] = \
+    cdf, sample_sizes, variances = \
         mlmc_simulator.simulate(epsilon=2.5e-2,
                                 initial_sample_size=initial_sample_size)
 
@@ -217,10 +312,7 @@ def test_sim_cdf_from_data(initial_sample_size):
     assert np.all(cdf >= 0.)
 
     # Verify that cdf is monotonic.
-    for i in range(1, len(cdf)):
-
-        if cdf[i] < cdf[i-1]:
-            assert False
+    assert np.all(cdf[1: -1] >= cdf[0: -2])
 
     # Verify that area under function sums to one.
     cdf_sum = np.sum(cdf[1:-1] - cdf[:-2])
