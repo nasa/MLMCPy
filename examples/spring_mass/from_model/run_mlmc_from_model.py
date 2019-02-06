afb849d11e925042a3034fac5ae731e2b1b1518a
jwarner308 <james.e.warner@nasa.gov>
2019-02-06 17:01:39 -0500

    Updated spring mass:from_model example script

--- examples/spring_mass/from_model/run_mlmc_from_model.py
+++ examples/spring_mass/from_model/run_mlmc_from_model.py
@@ -12,6 +12,7 @@ displacement using multi-level Monte Carlo. Here, we use Model and RandomInput
 objects with functional forms as inputs to MLMCPy. See the
 /examples/spring_mass/from_data/ for an example of using precomputed data
 in files as inputs.
+p
 '''
 
 
@@ -27,46 +28,55 @@ stiffness_distribution = RandomInput(distribution_function=beta_distribution,
                                      shift=1.0, scale=2.5, alpha=3., beta=2.,
                                      random_seed=1)
 
+# Step 2: Run standard Monte Carlo to generate a reference solution and target 
+# precision
+num_samples = 5000
+model = SpringMassModel(mass=1.5, time_step=0.01)
+input_samples = stiffness_distribution.draw_samples(num_samples)
+output_samples_mc = np.zeros(num_samples)
 
-# Step 2 - Initialize spring-mass models. Here using three levels with MLMC.
-# defined by different time steps
+start_mc = timeit.default_timer()
+
+for i, sample in enumerate(input_samples):
+    output_samples_mc[i] = model.evaluate([sample])
+
+mc_total_cost = timeit.default_timer() - start_mc
+
+mean_mc = np.mean(output_samples_mc)
+precision_mc = (np.var(output_samples_mc) / float(num_samples))
+print "Target precision: ", precision_mc
+
+# Step 3 - Initialize spring-mass models for MLMC. Here using three levels 
+# with MLMC defined by different time steps
 model_level1 = SpringMassModel(mass=1.5, time_step=1.0)
 model_level2 = SpringMassModel(mass=1.5, time_step=0.1)
 model_level3 = SpringMassModel(mass=1.5, time_step=0.01)
 
 models = [model_level1, model_level2, model_level3]
 
-# Step 3 - Initialize MLMC & predict max displacement to specified error.
-
+# Step 4 - Initialize MLMC & predict max displacement to specified precision
 mlmc_simulator = MLMCSimulator(stiffness_distribution, models)
 
 start_mlmc = timeit.default_timer()
 
 [estimates, sample_sizes, variances] = \
-    mlmc_simulator.simulate(epsilon=1e-1,
+    mlmc_simulator.simulate(epsilon=np.sqrt(precision_mc),
                             initial_sample_sizes=100,
                             verbose=True)
 
 mlmc_total_cost = timeit.default_timer() - start_mlmc
+
+#Step 5 - summarize results:
 
 print
 print 'MLMC estimate: %s' % estimates[0]
 print 'MLMC precision: %s' % variances[0]
 print 'MLMC total cost: %s' % mlmc_total_cost
 
-# Step 4: Run standard Monte Carlo to achieve similar variance for comparison.
-num_samples = 855
-input_samples = stiffness_distribution.draw_samples(num_samples)
-output_samples = np.zeros(num_samples)
-
-start_mc = timeit.default_timer()
-
-for i, sample in enumerate(input_samples):
-    output_samples[i] = model_level3.evaluate([sample])
-
-mc_total_cost = timeit.default_timer() - start_mc
-
 print
-print "MC estimate: %s" % np.mean(output_samples)
-print "MC precision: %s" % (np.var(output_samples) / float(num_samples))
+print "MC # samples: %s" % num_samples
+print "MC estimate: %s" % mean_mc
+print "MC precision: %s" % precision_mc
 print "MC total cost: %s" % mc_total_cost
+print
+print "MLMC computational speedup: %s" %  (mc_total_cost / mlmc_total_cost)
