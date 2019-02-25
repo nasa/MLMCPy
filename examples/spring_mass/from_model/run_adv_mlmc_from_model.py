a3216d5555fec3cee46f18c691ef93fd01c82620
Juan Barrientos <juan.m.barrientos@nasa.gov>
2019-02-25 16:26:09 -0500

    Reformatting pass, removed unused imports

--- examples/spring_mass/from_model/run_adv_mlmc_from_model.py
+++ examples/spring_mass/from_model/run_adv_mlmc_from_model.py
@@ -1,4 +1,3 @@
-import timeit
 import numpy as np
 
 from spring_mass_model import SpringMassModel
@@ -6,9 +5,9 @@ from MLMCPy.input import RandomInput
 from MLMCPy.mlmc import MLMCSimulator
 
 """
-This script demonstrates the modularity of the MLMCPy Simulator for simulating a 
+This script demonstrates the modularity of the MLMCPy Simulator by simulating a
 spring-mass system with a random spring stiffness to estimate the expected value
-of the maximum displacement using multi-level Monte Carlo. Here, we use Model 
+of the maximum displacement using multi-level Monte Carlo. Here, we use Model
 and RandomInput objects with functional forms as inputs to MLMCPy. See the
 /examples/spring_mass/from_data/ for an example of using precomputed data
 in files as inputs.
@@ -25,7 +24,7 @@ stiffness_distribution = RandomInput(distribution_function=beta_distribution,
                                      shift=1.0, scale=2.5, alpha=3., beta=2.,
                                      random_seed=1)
 
-# Step 2 - Initialize spring-mass models for MLMC. Here using three levels 
+# Step 2 - Initialize spring-mass models for MLMC. Here using three levels
 # with MLMC defined by different time steps:
 model_level1 = SpringMassModel(mass=1.5, time_step=1.0)
 model_level2 = SpringMassModel(mass=1.5, time_step=0.1)
@@ -63,19 +62,19 @@ print 'Estimates: ', estimates
 print 'Sample count: ', sample_count
 print 'Variances: ', variances
 
-# Step 5 - Run the model on each level the specified number of times in 
+# Step 5 - Run the model on each level the specified number of times in
 # sample_sizes to calculate the output differences for levels greater than 1
 
 # output_diffs_per_level = []
 
 # for level, model in enumerate(models):
-    
+
 #     sample_size = sample_sizes[level]
 #     output_diffs = np.zeros((sample_size))
 #     stiffness_samples = stiffness_distribution.draw_samples(sample_size)
 
 #     for i, sample in enumerate(stiffness_samples):
-    
+
 #         if level == 0:
 #             output_diffs[i] = model.evaluate([sample])
 #         else:
