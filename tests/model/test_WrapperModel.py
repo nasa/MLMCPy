d719d497d6c3c568e88e1c9ab381a78239cfbd70
Juan Barrientos <juan.m.barrientos@nasa.gov>
2019-02-25 11:51:54 -0500

    Removed check for cost attribute

--- tests/model/test_WrapperModel.py
+++ tests/model/test_WrapperModel.py
@@ -7,15 +7,9 @@ class TestCostClass:
     def __init__(self, cost=10):
         self.cost = cost
 
-def test_cost_attribute_initialization():
-    cost_class = TestCostClass()
-    wrapper_model = WrapperModel(cost_class)
-
-    assert isinstance(wrapper_model.cost, int)
-
 def test_attach_model_exception():
     cost_class = TestCostClass()
-    wrapper_model = WrapperModel(cost_class)
+    wrapper_model = WrapperModel()
 
     with pytest.raises(NotImplementedError):
         wrapper_model.attach_model(cost_class)
