b0896b0437b933f3701bc1fb03e8163c565abe8f
Juan Barrientos <juan.m.barrientos@nasa.gov>
2019-02-27 08:45:48 -0500

    Added MC, refactor pass

--- examples/spring_mass/from_model/run_adv_mlmc_from_model.py
+++ examples/spring_mass/from_model/run_adv_mlmc_from_model.py
@@ -1,3 +1,4 @@
+import timeit
 import numpy as np
 
 from spring_mass_model import SpringMassModel
@@ -24,7 +25,25 @@ stiffness_distribution = RandomInput(distribution_function=beta_distribution,
                                      shift=1.0, scale=2.5, alpha=3., beta=2.,
                                      random_seed=1)
 
-# Step 2 - Initialize spring-mass models for MLMC. Here using three levels
+# Step 2 - Run standard Monte Carlo to generate a reference solution and target
+# precision
+num_samples = 5000
+model = SpringMassModel(mass=1.5, time_step=0.01)
+input_samples = stiffness_distribution.draw_samples(num_samples)
+output_samples_mc = np.zeros(num_samples)
+
+start_mc = timeit.default_timer()
+
+for i, sample in enumerate(input_samples):
+    output_samples_mc[i] = model.evaluate([sample])
+
+mc_total_cost = timeit.default_timer() - start_mc
+
+mean_mc = np.mean(output_samples_mc)
+precision_mc = (np.var(output_samples_mc) / float(num_samples))
+print "Target precision: ", precision_mc
+
+# Step 3 - Initialize spring-mass models for MLMC. Here using three levels
 # with MLMC defined by different time steps:
 model_level1 = SpringMassModel(mass=1.5, time_step=1.0)
 model_level2 = SpringMassModel(mass=1.5, time_step=0.1)
@@ -32,14 +51,16 @@ model_level3 = SpringMassModel(mass=1.5, time_step=0.01)
 
 models = [model_level1, model_level2, model_level3]
 
-# Step 3 - Initialize MLMCSimulator:
+# Step 4 - Initialize MLMCSimulator:
 mlmc_simulator = MLMCSimulator(stiffness_distribution, models)
 
-# Step 4 - Calculate optimal sample size for each level:
+# Step 5 - Calculate optimal sample size for each level:
 initial_sample_size = 100
-epsilon = np.sqrt(0.0017234)
+epsilon = np.sqrt(precision_mc)
 
 # Optional - Compute cost and variances of model:
+#start_mlmc = timeit.default_timer()
+
 costs, variances = \
     mlmc_simulator.compute_costs_and_variances(initial_sample_size)
 
@@ -54,6 +75,7 @@ print
 print 'Optimal sample sizes: ', np.array2string(sample_sizes)
 
 # Optional - Call simulate now using the sample_sizes:
+
 estimates, sample_count, variances = \
     mlmc_simulator.simulate(epsilon, sample_sizes=sample_sizes)
 
@@ -62,7 +84,7 @@ print 'Estimates: ', estimates
 print 'Sample count: ', sample_count
 print 'Variances: ', variances
 
-# Step 5 - Run the model on each level the specified number of times in
+# Step 6 - Run the model on each level the specified number of times in
 # sample_sizes to calculate the output differences for levels greater than 1
 
 output_diffs_per_level = []
@@ -83,13 +105,22 @@ for level, model in enumerate(models):
 
     output_diffs_per_level.append(output_diffs)
 
-# Step 6 - Aggregate model outputs to compute estimators:
+# Step 7 - Aggregate model outputs to compute estimators:
 estimates, variances = \
     mlmc_simulator.compute_estimators(output_diffs_per_level)
 
-# # Step 7 - Summarize results:
+#mlmc_total_cost = timeit.default_timer() - start_mlmc
 
+# # Step 8 - Summarize results:
 print
 print 'MLMC estimate: %s' % estimates[0]
 print 'MLMC precision: %s' % variances[0]
 # print 'MLMC total cost: %s' % mlmc_total_cost
+
+print
+print "MC # samples: %s" % num_samples
+print "MC estimate: %s" % mean_mc
+print "MC precision: %s" % precision_mc
+print "MC total cost: %s" % mc_total_cost
+print
+# print "MLMC computational speedup: %s" %  (mc_total_cost / mlmc_total_cost)
